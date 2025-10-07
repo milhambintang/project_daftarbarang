543d17c0b04cd0e915e09f6e9b3c057b6152ecff
rikarahmawati88 <rikarahmawati_2327240095@mhs.mdp.ac.id>
2025-10-07 12:54:55 +0700

    Initial commit

---
+++ bin/project_daftarbarang.dart
@@ -0,0 +1,7 @@
+void main() {
+  List<Map<String, dynamic>> daftarBarang = [
+    {'nama': 'Pensil', 'harga': 2000},
+    {'nama': 'Buku', 'harga': 5000},
+    {'nama': 'Penghapus', 'harga': 1000},
+  ];
+}
